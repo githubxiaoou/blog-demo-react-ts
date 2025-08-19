a0552494f34ffa6d71c08707ca1dcf6e0c3087ae
sunwenou <sunwenou@lrhealth.com>
2025-08-19 15:16:45 +0800

    #8 - Using State (useState hook)

--- dojo-blog/src/Home.tsx
+++ dojo-blog/src/Home.tsx
@@ -1,13 +1,11 @@
-const Home = () => {
-  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-    console.log("Button clicked!", e); // Handle button click
-  };
+import { useState } from "react";
 
-  const handleClickAgain = (
-    name: string,
-    e: React.MouseEvent<HTMLButtonElement>
-  ) => {
-    console.log("Button clicked again!", name, e); // Handle button click with name
+const Home = () => {
+  const [name, setName] = useState("Owen");
+  const [age, setAge] = useState(25);
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    setAge(age + 1);
+    setName("Owen Chang");
   };
 
   return (
@@ -16,9 +14,9 @@ const Home = () => {
       <p>Welcome to the home page of the Dojo Blog!</p>
       <p>Here you can find the latest posts and updates.</p>
       <button onClick={handleClick}>Click me</button>
-      <button onClick={(e) => handleClickAgain("mario", e)}>
-        Click me again
-      </button>
+      <p>
+        {name} is {age} years old.
+      </p>
     </div>
   );
 };
