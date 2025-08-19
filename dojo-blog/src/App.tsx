6f9aae03cdcecf0f91ab428be5799bb6bd3b5e47
sunwenou <sunwenou@lrhealth.com>
2025-08-19 15:03:53 +0800

    #5 - Multiple Components

--- dojo-blog/src/App.tsx
+++ dojo-blog/src/App.tsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './Navbar'
+import Home from './Home'
 
 function App() {
   const title = 'Welcome to the new blog';
@@ -11,18 +13,9 @@ function App() {
 
   return (
     <>
+      <Navbar />
       <div className="content">
-        <h1>{ title }</h1>
-        <p>Liked { likes } times</p>
-
-        {/* <p>{ person }</p> */}
-
-        <p>{ 10 }</p>
-        <p>{ "hello, ninjas" }</p>
-        <p>{ [1,2,3,4,5] }</p>
-        <p>{ Math.random() * 10 }</p>
-
-        <a href={link}>Google Site</a>
+        <Home />
       </div>
     </>
   )
---
+++ dojo-blog/src/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className="home">
+      <h2>Home Page</h2>
+      <p>Welcome to the home page of the Dojo Blog!</p>
+      <p>Here you can find the latest posts and updates.</p>
+    </div>
+  )
+}
+
+export default Home
