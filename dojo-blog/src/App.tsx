b31faec93bf88fc0812bc8bd479dc1cf9360336a
sunwenou <sunwenou@lrhealth.com>
2025-08-20 13:54:54 +0800

    #21 - The React Router

--- dojo-blog/src/App.tsx
+++ dojo-blog/src/App.tsx
@@ -1,21 +1,18 @@
-
-import Navbar from './Navbar'
-import Home from './Home'
+import Navbar from "./Navbar";
+import Home from "./Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
-  const title = 'Welcome to the new blog';
-  const likes = 50;
-  // const person = { name: 'yoshi', age: 30 };
-  const link = 'http://www.google.com';
-
   return (
-    <>
+    <Router>
       <Navbar />
       <div className="content">
-        <Home />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
       </div>
-    </>
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;
