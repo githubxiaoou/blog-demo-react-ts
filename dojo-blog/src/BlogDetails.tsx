51dde34a678fe2b30abc640e919a06e2981806b1
sunwenou <sunwenou@lrhealth.com>
2025-08-21 16:42:42 +0800

    #31 - Deleting Blogs

--- dojo-blog/src/BlogDetails.tsx
+++ dojo-blog/src/BlogDetails.tsx
@@ -1,10 +1,20 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import type { Blog } from "./Home";
 import useFetch from "./useFetch";
 
 const BlogDetails = () => {
   const { id } = useParams();
   const { data: blog } = useFetch<Blog>(`http://localhost:4000/blogs/${id}`);
+  const navigate = useNavigate();
+
+  function handleDelete(id: number): void {
+    fetch(`http://localhost:4000/blogs/${id}`, {
+      method: "DELETE",
+    }).then(() => {
+      console.log("Blog deleted");
+      navigate("/");
+    });
+  }
 
   return (
     <div className="blog-details">
@@ -13,6 +23,7 @@ const BlogDetails = () => {
           <h2>{blog.title}</h2>
           <p>{blog.body}</p>
           <p>Written by {blog.author}</p>
+          <button onClick={() => handleDelete(blog.id)}>delete</button>
         </>
       ) : (
         <div>Loading...</div>
