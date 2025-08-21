a99e49ddd4acdaa22807ad036fcd14a6e2cfaa64
sunwenou <sunwenou@lrhealth.com>
2025-08-21 16:04:50 +0800

    #27 - Controlled Inputs (forms)

--- dojo-blog/src/Create.tsx
+++ dojo-blog/src/Create.tsx
@@ -1,7 +1,40 @@
-import React from "react";
+import { useState } from "react";
 
 const Create = () => {
-  return <div className="create">Add a new blog</div>;
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("mario");
+
+  return (
+    <div className="create">
+      <h2>Add a new blog</h2>
+      <form>
+        <label>
+          Blog title:
+          <input
+            type="text"
+            required
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </label>
+        <label>
+          Blog body:
+          <textarea
+            required
+            value={body}
+            onChange={(e) => setBody(e.target.value)}
+          />
+        </label>
+        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+          <option value="mario">Mario</option>
+          <option value="luigi">Luigi</option>
+          <option value="peach">Peach</option>
+        </select>
+        <button>Add Blog</button>
+      </form>
+    </div>
+  );
 };
 
 export default Create;
