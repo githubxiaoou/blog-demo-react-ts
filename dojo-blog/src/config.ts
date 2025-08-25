2c74eadf721634d3c4680ed39a1d84076ee59d8b
sunwenou <sunwenou@lrhealth.com>
2025-08-25 12:46:03 +0800

    add render base url

--- dojo-blog/src/config.ts
+++ dojo-blog/src/config.ts
@@ -1,1 +1,2 @@
-export const API_BASE_URL = "https://68a82271bb882f2aa6dd8b17.mockapi.io";
+// export const API_BASE_URL = "https://68a82271bb882f2aa6dd8b17.mockapi.io";
+export const API_BASE_URL = "https://json-server-render-s9kv.onrender.com";
